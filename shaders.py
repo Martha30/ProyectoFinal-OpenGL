89bd9ed08c1b768123491920356674c359c4953a
Martha30 <53914325+Martha30@users.noreply.github.com>
2021-11-27 11:55:02 -0600

    clase de los shaders

---
+++ shaders.py
@@ -0,0 +1,39 @@
+#Universidad del Valle de Guatemala
+#Laurelinda Gómez
+# 26/11/2021
+#Los shaders
+
+vertex_shader = """
+#version 460
+layout (location = 0) in vec4 pos;
+layout (location = 1) in vec4 normal;
+layout (location = 2) in vec2 texcoords;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform vec4 color;
+uniform vec4 light;
+uniform float ambient;
+out vec4 vertexColor;
+out vec2 vertexTexcoords;
+void main()
+{
+    float intensity = dot(model * normal, normalize(light - pos));
+    gl_Position = projection * view * model * pos;
+    vertexColor = vec4(1, 1, 1, 1) * ambient + color * intensity;
+    vertexTexcoords = texcoords;
+}
+"""
+
+
+fragment_shader = """
+#version 460
+layout (location = 0) out vec4 diffuseColor;
+in vec4 vertexColor;
+in vec2 vertexTexcoords;
+uniform sampler2D tex;
+void main()
+{
+    diffuseColor =  vertexColor * texture(tex, vertexTexcoords);
+}
+"""
